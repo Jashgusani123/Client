9baf6a2a4a3f5c495a7a59458e9b88e8d3801bf4
JASH GUSANI~ <jashgusani20@gmail.com>
2024-09-20 14:47:21 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['mock-aws-s3', 'aws-sdk', 'nock', '@mapbox/node-pre-gyp'],
+      external: [
+        'mock-aws-s3',
+        'aws-sdk',
+        'nock',
+        '@mapbox/node-pre-gyp',
+        '@mui/icons-material' // Add Material-UI icons as external
+      ],
     },
   },
   resolve: {
@@ -11,4 +18,8 @@ export default defineConfig({
       'node-pre-gyp': false,
     },
   },
+  optimizeDeps: {
+    include: ['@mui/icons-material'], // Ensure Material-UI icons are included in dependencies
+  },
+  plugins: [react()],
 });
